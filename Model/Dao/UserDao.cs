118d43346ff2bc6520278952edbcb3eb4ce36f53
nah-coder <huyanhnguyen1211@gmail.com>
2024-12-03 21:43:58 +0700

    back

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,115 @@
+﻿using Model.FrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList.Mvc;
+
+using PagedList;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDBContext db = null;
+        public UserDao()
+        {
+            db = new OnlineShopDBContext();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public bool Update(User entity)
+        {
+            try
+            {
+                var user = db.Users.Find(entity.ID);
+                user.Name = entity.Name;
+                user.UserName = entity.UserName;
+                if (!string.IsNullOrEmpty(entity.Name))
+                {
+                    user.Password = entity.Password;
+                }
+                user.Address = entity.Address;
+                user.Email = entity.Email;
+                user.ModifiedBy = entity.ModifiedBy;
+                user.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.Users.Find(id);
+                db.Users.Remove(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public User ViewDetail(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public IEnumerable<User> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<User> model = db.Users;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.UserName.Contains(searchString) || x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreateDate).ToPagedList(page, pageSize);
+        }
+
+        public User GetById(string username)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == username);
+        }
+
+        public int Login(string userName, string passWord)
+        {
+            var result = db.Users.SingleOrDefault(x => x.UserName == userName);
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (result.Status == false)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (result.Password == passWord)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -2;
+                    }
+                }
+            }
+        }
+    }
+}
